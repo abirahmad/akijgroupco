583330347f88fa9d68cae79d15f8fc1df41a9dde
abirahmad <abir.ahmad007@gmail.com>
2024-06-26 01:49:27 +0600

    updated

--- src/components/layouts/Slider.jsx
+++ src/components/layouts/Slider.jsx
@@ -22,7 +22,7 @@ const Carousel = () => {
                     <Image src="/slider1.jpg" alt="Slider 2" width={1920} height={800} />
                     <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-50">
                         <h2 className="title1 md:text-sm lg:text-4xl font-bold text-white">The Best Business Information</h2>
-                        <h1 className="title2 md:text-sm lg:text-5xl font-bold text-white">We're In The Business Of Helping You Start Your Business</h1>
+                        <h1 className="title2 md:text-sm lg:text-5xl font-bold text-white">We are In The Business Of Helping You Start Your Business</h1>
                         <div className="flex justify-center mt-4">
                             <a className="ready-btn right-btn page-scroll mx-2 py-2 px-4 bg-blue-500 text-white rounded" href="#services">See Services</a>
                             <a className="ready-btn page-scroll mx-2 py-2 px-4 bg-gray-500 text-white rounded" href="#about">Learn More</a>
@@ -33,7 +33,7 @@ const Carousel = () => {
                     <Image src="/slider2.jpg" alt="Slider 2" width={1920} height={1080} />
                     <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-50">
                         <h2 className="title1 md:text-sm lg:text-4xl font-bold text-white">The Best Business Information</h2>
-                        <h1 className="title2 md:text-sm lg:text-4xl font-bold text-white">We're In The Business Of Helping You Start Your Business</h1>
+                        <h1 className="title2 md:text-sm lg:text-4xl font-bold text-white">We are In The Business Of Helping You Start Your Business</h1>
                         <div className="flex justify-center mt-4">
                             <a className="ready-btn right-btn page-scroll mx-2 py-2 px-4 bg-blue-500 text-white rounded" href="#services">See Services</a>
                             <a className="ready-btn page-scroll mx-2 py-2 px-4 bg-gray-500 text-white rounded" href="#about">Learn More</a>
@@ -44,7 +44,7 @@ const Carousel = () => {
                     <Image src="/slider3.jpg" alt="Slider 3" width={1920} height={1080} />
                     <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-50">
                         <h2 className="title1 md:text-sm lg:text-4xl font-bold text-white">The Best Business Information</h2>
-                        <h1 className="title2 md:text-sm lg:text-4xl font-bold text-white">We're In The Business Of Helping You Start Your Business</h1>
+                        <h1 className="title2 md:text-sm lg:text-4xl font-bold text-white">We are In The Business Of Helping You Start Your Business</h1>
                         <div className="flex justify-center mt-4">
                             <a className="ready-btn right-btn page-scroll mx-2 py-2 px-4 bg-blue-500 text-white rounded" href="#services">See Services</a>
                             <a className="ready-btn page-scroll mx-2 py-2 px-4 bg-gray-500 text-white rounded" href="#about">Learn More</a>
